f5fa45332d70afce5de2cb6e3ed5e717e9161892
Hao Wang <hao.1.wang@gmail.com>
2015-04-08 23:06:38 -0400

    mess around angular with mysql which is not correct for Angular is front end. will need backend nodejs service.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+/*angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.cooking',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+*/
+angular.module('cooking', ['ngRoute', 'cooking']).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/cooking'});
 }]);
--- app/cooking/cooking.js
+++ app/cooking/cooking.js
@@ -1,17 +1,47 @@
 'use strict';
 
-angular.module('myApp.cooking', ['ngRoute'])
+var cooking = angular.module('cooking', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/cooking', {
-    templateUrl: 'cooking/cooking.html',
-    controller: 'CookingCtrl'
+cooking.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/register', {
+    templateUrl: 'cooking/register.html',
+    controller: 'CookingRegisterCtrl'
   });
-}])
+}]);
 
-.controller('CookingCtrl', function ($scope) {
+cooking.controller('CookingRegisterCtrl', ['$scope', function ($scope) {
+
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+  	host: "localhost",
+  	user: "root",
+  	password: "root",
+  	database: "cooking"
+  });
+  connection.connect(function(err) {
+    if(err) {
+      console.log("Problem with MySQL : " + err);
+    } else {
+      console.log("Connected with Database : ");
+    }
+  });
+
+
+  //  connection.query("SELECT * from user_info", function(err, rows) {
   $scope.addUser = function (user) {
+    $scope.user = user;
     console.log(user.name);
+    connection.query("insert into user_info value(user.name, user.password, user.email)", function(err, rows) {
+      if(err) {
+        console.log("Problem with MySQL : " + err);
+      } else {
+        console.log("Connected with Database : ");
+      }
+    });
   };
-});
-
+  $scope.showUser = function (user) {
+    connection.query("SELECT * from user_info", function(err, rows) {
+      $scope.users = rows;
+    });
+  };
+}]);
